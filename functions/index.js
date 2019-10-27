3f232ed30f2f7b42438d4edadd0ed289200f6a99
bombnp <bomb.NP@gmail.com>
2019-10-27 12:59:59 +0700

    Reverted to pre-firebase repo

--- functions/index.js
+++
@@ -1,8 +0,0 @@
-const functions = require('firebase-functions');
-const admin = require("firebase-admin")
-
-admin.initializeApp();
-
-const app = require("./server.js");
-
-module.exports.app = functions.runWith({ timeoutSeconds : 120}).https.onRequest(app);
